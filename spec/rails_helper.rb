cbbe028d6f9a056a74d6b403a40be2f4ae950a00
valedithat <valedithat@pm.me>
2022-02-06 18:37:01 -0500

    update rails_helper, add simplecov

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,10 +6,8 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 
 require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
-require 'pry'
 require 'database_cleaner/active_record'
 require 'capybara/rspec'
-
 
 Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
 
@@ -23,34 +21,13 @@ end
 
 
 RSpec.configure do |config|
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  # config.fixture_path = "#{::Rails.root}/spec/fixtures"\
-
   # factory_bot_rails, use factory bot methods
   config.include FactoryBot::Syntax::Methods
 
-  # If not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, assign false
+  # If run each of your examples within a transaction, assign false
   config.use_transactional_fixtures = true
 
-  # You can uncomment this line to turn off ActiveRecord support entirely.
-  # config.use_active_record = false
-
-  # RSpec Rails can automatically mix in different behaviours to your tests
-  # based on their file location, for example enabling you to call `get` and
-  # `post` in specs under `spec/controllers`.
-  #
-  # You can disable this behaviour by removing the line below, and instead
-  # explicitly tag your specs with their type, e.g.:
-  #
-  #     RSpec.describe UsersController, type: :controller do
-  #       # ...
-  #     end
-  #
-  # The different available types are documented in the features, such as in
-  # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
-
   # Filter lines from Rails gems in backtraces.
   config.filter_rails_from_backtrace!
 end
