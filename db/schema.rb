3b53c9cbd30cbb09fb8f25dfb6b35301faec7032
valedithat <valedithat@pm.me>
2022-02-17 00:48:54 -0500

    create articles migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_07_002955) do
+ActiveRecord::Schema.define(version: 2022_02_17_053116) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.text "body"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_articles_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -41,4 +51,5 @@ ActiveRecord::Schema.define(version: 2022_02_07_002955) do
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
 
+  add_foreign_key "articles", "users"
 end
