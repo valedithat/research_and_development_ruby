a69396e660373550be326dc2c9f3f9fb61ec4847
valedithat <valedithat@pm.me>
2022-02-16 23:41:16 -0500

    validates name of user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable, :confirmable, :lockable, :timeoutable, :omniauthable
+
+  validates :name
 end
