a9f4e023e212eca1dcedd20044db4c0c6e001f43
valedithat <valedithat@pm.me>
2022-02-17 00:09:59 -0500

    update rails_helper -- rails not found

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,12 +1,11 @@
-ENV['RAILS_ENV'] ||= 'test'
-require 'rspec/rails'
 require 'spec_helper'
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../config/environment', __dir__)
 
 # Prevent database truncation if the environment is production
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 
-require File.expand_path('../config/environment', __dir__)
-
+require 'rspec/rails'
 require 'database_cleaner/active_record'
 require 'capybara/rspec'
 
