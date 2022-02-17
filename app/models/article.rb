8b6ba0cbeabe4e412c6bc1522ec28efa4d794717
valedithat <valedithat@pm.me>
2022-02-17 00:49:22 -0500

    update articles and article_spec with schema info

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,23 @@
+# == Schema Information
+#
+# Table name: articles
+#
+#  id          :integer          not null, primary key
+#  body        :text
+#  description :string
+#  title       :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  user_id     :integer          not null
+#
+# Indexes
+#
+#  index_articles_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  user_id  (user_id => users.id)
+#
 class Article < ApplicationRecord
   belongs_to :user
 end
